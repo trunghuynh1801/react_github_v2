97320aa35851805317246b4a34895740c7abd7b6
Huỳnh Tấn Trung <huynhtantrung1801@gmail.com>
2023-12-23 15:35:33 +0700

    them button

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -6,69 +6,77 @@ const DataTableFromAPI = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://us-east-1.aws.data.mongodb-api.com/app/agg_func-voayj/endpoint/getdata_forREACT" // Thay URL_API_ENDPOINT bằng đường dẫn đến API của bạn
-        );
-        setData(response.data);
-        setError(null);
-      } catch (error) {
-        console.error("Lỗi khi lấy dữ liệu:", error);
-        setError("Không thể tải dữ liệu. Vui lòng thử lại sau.");
-      } finally {
-        setLoading(false);
-      }
-    };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://us-east-1.aws.data.mongodb-api.com/app/agg_func-voayj/endpoint/getdata_forREACT"
+      );
+      setData(response.data);
+      setError(null);
+    } catch (error) {
+      console.error("Lỗi khi lấy dữ liệu:", error);
+      setError("Không thể tải dữ liệu. Vui lòng thử lại sau.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     fetchData();
   }, []);
+
+  const handleRefresh = () => {
+    setLoading(true);
+    fetchData();
+  };
 
   return (
     <div>
       <h2 style={{ fontSize: "1.5em" }}>DANH SÁCH DỮ LIỆU</h2>
       <div style={{ marginBottom: "10px" }}>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <table
-            style={{
-              width: "100%",
-              borderCollapse: "collapse",
-              marginTop: "10px",
-            }}
-          >
-            <thead>
-              <tr>
-                <th>STT</th>
-                <th>Desire</th>
-                <th>Distance</th>
-                <th>Setpoint</th>
-                <th>Hall</th>
-                <th>Current</th>
-                <th>Last Balance</th>
-                <th>Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((item, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{item.public.input.jsonData.desire}</td>
-                  <td>{item.public.input.jsonData.distance}</td>
-                  <td>{item.public.output.jsonData.setpoint}</td>
-                  <td>{item.public.input.jsonData.hall}</td>
-                  <td>{item.public.output.jsonData.current}</td>
-                  <td>{item.public.input.jsonData.last_balance}</td>
-                  <td>{item.public.input.jsonInfo.time}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-        {error && <p style={{ color: "red" }}>{error}</p>}
+        <button onClick={handleRefresh} disabled={loading}>
+          {loading ? "Refreshing..." : "Refresh"}
+        </button>
       </div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <table
+          style={{
+            width: "100%",
+            borderCollapse: "collapse",
+            marginTop: "10px",
+          }}
+        >
+          <thead>
+            <tr>
+              <th>STT</th>
+              <th>Desire</th>
+              <th>Distance</th>
+              <th>Setpoint</th>
+              <th>Hall</th>
+              <th>Current</th>
+              <th>Last Balance</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.public.input.jsonData.desire}</td>
+                <td>{item.public.input.jsonData.distance}</td>
+                <td>{item.public.output.jsonData.setpoint}</td>
+                <td>{item.public.input.jsonData.hall}</td>
+                <td>{item.public.output.jsonData.current}</td>
+                <td>{item.public.input.jsonData.last_balance}</td>
+                <td>{item.public.input.jsonInfo.time}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
 };
