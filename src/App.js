c191460d9a9ce13250e467c28d77240d365c5d34
Huỳnh Tấn Trung <huynhtantrung1801@gmail.com>
2024-01-04 00:37:22 +0700

    add warning square

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Form from "./components/Form";
 import UserList from "./components/DataTable";
+import Warning from "./components/Warning";
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Magnetic Levitation</h1>
         <Form />
+        <Warning />
         <UserList />
       </header>
     </div>
---
+++ src/components/Warning.jsx
@@ -0,0 +1,51 @@
+// WarningSquare.js
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const WarningSquare = () => {
+  const [warningData, setWarningData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://us-east-1.aws.data.mongodb-api.com/app/agg_func-voayj/endpoint/getdata_forREACT"
+        );
+        setWarningData(response.data.public.input.jsonData.warning);
+      } catch (error) {
+        console.error("Lỗi khi lấy dữ liệu:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const getSquareColor = () => {
+    return warningData === 1 ? "red" : "green";
+  };
+
+  const getSquareContent = () => {
+    return warningData === 1 ? "Fail !" : "Fly :) ";
+  };
+
+  return (
+    <div
+      style={{
+        width: "100px",
+        height: "100px",
+        backgroundColor: getSquareColor(),
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+        fontWeight: "bold",
+        fontSize: "18px",
+      }}
+    >
+      {getSquareContent()}
+    </div>
+  );
+};
+
+export default WarningSquare;
