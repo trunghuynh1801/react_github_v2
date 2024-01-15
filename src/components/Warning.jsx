09abd28eedbef208ac0fbfd4cdbe5af7ad73b745
Huỳnh Tấn Trung <huynhtantrung1801@gmail.com>
2024-01-16 01:14:15 +0700

    canh giua

--- src/components/Warning.jsx
+++ src/components/Warning.jsx
@@ -45,6 +45,10 @@ const WarningSquare = () => {
   return (
     <div
       style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
         width: "100px",
         height: "100px",
         backgroundColor: getSquareColor(),
