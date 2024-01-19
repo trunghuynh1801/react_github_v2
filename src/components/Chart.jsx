cad4349f270a69119738c2b7c18ce2d1b5740956
Huỳnh Tấn Trung <huynhtantrung1801@gmail.com>
2024-01-19 12:25:59 +0700

    tang chieu cao chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -7,7 +7,7 @@ const MongoDBChart = () => {
       <Iframe
         url="https://charts.mongodb.com/charts-immabeo-swlvx/public/dashboards/bf2dc2cb-a9c9-4e3f-b648-cab61ad29d89"
         width="100%"
-        height="600px"
+        height="1000px"
         id="mongodb-chart-iframe"
         display="initial"
         position="relative"
